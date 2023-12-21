b7198c307ff5529e14c86b3aae176114368d7098
yasirakbar17 <139375381+yasirakbar17@users.noreply.github.com>
2023-12-21 14:03:12 +0800

    Add files via upload

---
+++ mysql _to_python.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import mysql.connector
+import pandas as pd
+
+database_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'a1d2g3j4m5',
+    'database': 'belajar_sql'
+}
+
+koneksi = mysql.connector.connect(**database_config) #membuat koneksi
+cursor = koneksi.cursor()
+query = "SELECT * FROM NAMA TABEL" #nama tabel
+cursor.execute(query)
+column = [description[0] for description in cursor.description]
+
+df = pd.DataFrame(cursor.fetchall(), columns=column) # Membuat DataFrame
+
+cursor.close()
+conn.close()  # Menutup koneksi
+
+dfcolumn_names = [description[0] for description in cursor.description]  # Menampilkan DataFrame
+
+df = pd.DataFrame(cursor.fetchall(), columns=column_names) # Membuat DataFrame
+
+cursor.close() 
+conn.close()  # Menutup koneksi
+
